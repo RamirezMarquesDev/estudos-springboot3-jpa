90f968a6e138395e4fa5813f39356301a96846c4
RamirezMarquesDev <ramirezmarquescampello@gmail.com>
2024-02-25 15:50:37 -0300

    Criado Service layer e component resgistratio

--- estudo/src/main/java/com/ramirez/estudo/resourses/UserResourse.java
+++ estudo/src/main/java/com/ramirez/estudo/resourses/UserResourse.java
@@ -1,19 +1,32 @@
 package com.ramirez.estudo.resourses;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ramirez.estudo.entities.User;
+import com.ramirez.estudo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResourse {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
-		return ResponseEntity.ok().body(u);		
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);		
+	}
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
